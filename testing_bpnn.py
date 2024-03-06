f3471a52ef6ee3530a593b539796686f3abb7f88
rizkyrivaldi <rizkyrivaldi00@gmail.com>
2024-03-07 05:35:04 +0700

    added bpnn

---
+++ testing_bpnn.py
@@ -0,0 +1,11 @@
+from utils.NN_numpy import BPNN
+
+a = BPNN([3, 2, 3])
+
+# print(a.weight)
+
+a.feedForward([0.5, 0.7, 0.3])
+# print(a.predicted)
+# print(len(a.weight))
+
+a.backPropagation([1, 2, 3])
